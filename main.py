bfcff6fc32bc8df69dd043b431aa78ef8b2db56e
Abhinav <AbhinavSunil@hotmail.com>
2026-06-25 09:22:31 +0530

    Feat : added the fastapi configs..

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from aeromind-ai-service!")
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title="Aeromind AI Service",
+    description=("Ai powered flight search",
+                 "Using groq api key for nlp task"
+                 ),
+                 version="0.1.0",
+                 docs_url="/docs",
+                 redoc_url="/redoc"
+
+)
+
+
+
+app.add_middleware(CORSMiddleware,
+                   alllow_origins=["*"],
+                   alllow_credentials = True,
+                   alllow_methods=["*"],
+                   alllow_headers=["*"],
+                   )
+
+
+
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
