efc9c33fa475dfdf1d865ffff49fa39fb9eee81c
Abhinav <AbhinavSunil@hotmail.com>
2026-06-25 09:30:51 +0530

    Feat : added conversion to dictionary

--- app/errors/base.error.py
+++ app/errors/base.error.py
@@ -1,7 +1,16 @@
 class ErrorHandler(Exception):
+
     def __init__(self, message:str,status_code:int,detail:str | None = None):
         super().__init__(message)
         self.message = message
         self.status_code = status_code
         self.detail = detail
-        
+
+    def to_dictionary(self)->dict:
+        payload:dict = {
+            "error":self.message,
+            "status_code":self.status_code
+        }
+        if self.detail :
+            payload["detail"] = self.detail
+        return payload
